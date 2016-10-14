374fe25b6f8a719a79d7b327715718414f328092
Ondrej <ondrej.k@lantonmedia.com>
2016-10-15 01:19:04 +0200

    Update ActionBar title from fragments

--- app/src/main/java/sk/beacode/beacodeapp/fragments/SearchEventsFragment.java
+++ app/src/main/java/sk/beacode/beacodeapp/fragments/SearchEventsFragment.java
@@ -1,6 +1,12 @@
 package sk.beacode.beacodeapp.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import org.androidannotations.annotations.EFragment;
 
@@ -8,4 +14,14 @@ import sk.beacode.beacodeapp.R;
 
 @EFragment(R.layout.fragment_search_events)
 public class SearchEventsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.title_fragment_search_events);
+        }
+
+        return null;
+    }
 }
