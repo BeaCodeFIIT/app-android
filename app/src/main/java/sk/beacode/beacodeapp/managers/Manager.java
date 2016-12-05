fd2b17e3fdc5cfbdad0eb443ce2ac5912e571b5d
Ondrej <ondrej.kipila@gmail.com>
2016-12-05 19:02:01 +0100

    Use IP address for API_ROOT_URL, IMAGE_ROOT_URL

--- app/src/main/java/sk/beacode/beacodeapp/managers/Manager.java
+++ app/src/main/java/sk/beacode/beacodeapp/managers/Manager.java
@@ -1,6 +1,6 @@
 package sk.beacode.beacodeapp.managers;
 
 public class Manager {
-    static final String API_ROOT_URL = "http://team06-16.studenti.fiit.stuba.sk/beacode_dev/current/web/app.php/api/app/";
-    public static final String IMAGE_ROOT_URL = "http://team06-16.studenti.fiit.stuba.sk/beacode_dev";
+    static final String API_ROOT_URL = "http://147.175.149.218/beacode_dev/current/web/app.php/api/app/";
+    public static final String IMAGE_ROOT_URL = "http://147.175.149.218/beacode_dev";
 }
